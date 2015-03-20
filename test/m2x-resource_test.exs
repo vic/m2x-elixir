ad656ea5aba4c3f9e819bf36fe61f18f91ebcc01
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2015-03-20 11:59:37 -0700

    Test M2X.Key with tests in M2X.ResourceTest.Common.

--- test/m2x-resource_test.exs
+++ test/m2x-resource_test.exs
@@ -35,7 +35,7 @@ defmodule M2X.ResourceTest.Common do
         client = MockEngine.client \
           {:post, main_path, %{}},
           {204, new_test_attributes}
-        subject = M2X.Device.create!(client)
+        subject = TheModule.create!(client)
 
         assert subject.client     == client
         assert subject.attributes == new_test_attributes
@@ -45,7 +45,7 @@ defmodule M2X.ResourceTest.Common do
         client = MockEngine.client \
           {:post, main_path, test_attributes},
           {204, new_test_attributes}
-        subject = M2X.Device.create!(client, test_attributes)
+        subject = TheModule.create!(client, test_attributes)
 
         assert subject.client     == client
         assert subject.attributes == new_test_attributes
@@ -56,7 +56,7 @@ defmodule M2X.ResourceTest.Common do
           {:get, path, nil},
           {200, new_test_attributes}
         assert subject.attributes == test_attributes
-        new_subject = M2X.Device.refreshed(subject)
+        new_subject = TheModule.refreshed(subject)
 
         assert new_subject.client == subject.client
         assert new_subject.attributes == new_test_attributes
@@ -66,14 +66,14 @@ defmodule M2X.ResourceTest.Common do
         subject = mock_subject \
           {:put, path, new_test_attributes},
           {204, nil}
-        assert M2X.Device.update!(subject, new_test_attributes).success?
+        assert TheModule.update!(subject, new_test_attributes).success?
       end
 
       test "delete!" do
         subject = mock_subject \
           {:delete, path, nil},
           {204, nil}
-        assert M2X.Device.delete!(subject).success?
+        assert TheModule.delete!(subject).success?
       end
 
     end
@@ -85,8 +85,19 @@ defmodule M2X.ResourceTest.Device do
   use M2X.ResourceTest.Common, mod: M2X.Device
   doctest M2X.Device
 
-  def id             do "a2852df27102179429b3a02641594044" end
-  def main_path      do "/v2/devices"  end
-  def path           do main_path<>"/"<>id   end
-  def required_attrs do %{ "id" => id } end
+  def id             do "0123456789abcdef0123456789abcdef" end
+  def main_path      do "/v2/devices"                      end
+  def path           do main_path<>"/"<>id                 end
+  def required_attrs do %{ "id" => id }                    end
+end
+
+defmodule M2X.ResourceTest.Key do
+  use ExUnit.Case
+  use M2X.ResourceTest.Common, mod: M2X.Key
+  doctest M2X.Key
+
+  def key            do "0123456789abcdef0123456789abcdef" end
+  def main_path      do "/v2/keys"                         end
+  def path           do main_path<>"/"<>key                end
+  def required_attrs do %{ "key" => key }                  end
 end
