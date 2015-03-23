66611ac9835a4c55d7598a451359358ef60e3472
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2015-03-23 15:20:29 -0700

    Update mix.exs for publishing as a Hex.pm package.

--- mix.exs
+++ mix.exs
@@ -4,20 +4,36 @@ defmodule M2X.Mixfile do
   def version, do: "0.0.1"
 
   def project do
-    [ app:     :m2x,
-      version: version,
-      elixir:  "~> 1.0",
-      deps:    deps ]
+    [ app:         :m2x,
+      version:     version,
+      elixir:      "~> 1.0",
+      deps:        dependencies,
+      description: description,
+      package:     package ]
   end
 
-  # Configuration for the OTP application
-  def application do
-    [ applications: [:logger] ]
-  end
+  def application do [] end
 
-  # Dependencies
-  defp deps do
+  defp dependencies do
     [ hackney: "~> 1.0",
       json:    "~> 0.3" ]
+  end
+
+  defp description do
+    """
+    Client library for the [AT&T M2X](http://m2x.att.com) API.
+    [AT&T M2X](http://m2x.att.com) is a cloud-based fully managed
+    time-series data storage service for network connected
+    machine-to-machine (M2M) devices and the Internet of Things (IoT).
+    """
+  end
+
+  defp package do
+    [ contributors: ["Joe McIlvain"],
+      licenses:     ["MIT"],
+      links: %{
+        "GitHub"   => "https://github.com/ericmj/postgrex",
+        "API Docs" => "https://m2x.att.com/developer/documentation"
+      } ]
   end
 end
